c5f17b748dfc37e681cdcebac4c1bdaa195deaa4
Christopher Gammie <contact@gammie.co.uk>
2020-11-16 13:27:20 +0000

    [Feature] Bring in rule objects

---
+++ src/Rules/HasOne.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Copyright 2020 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace LaravelJsonApi\Validation\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
+use LaravelJsonApi\Contracts\Schema\PolymorphicRelation;
+use LaravelJsonApi\Contracts\Schema\Schema;
+
+class HasOne implements Rule
+{
+
+    /**
+     * @var Schema
+     */
+    private Schema $schema;
+
+    /**
+     * @var array|null
+     */
+    private ?array $types;
+
+    /**
+     * HasOne constructor.
+     *
+     * @param Schema $schema
+     */
+    public function __construct(Schema $schema)
+    {
+        $this->schema = $schema;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        if (!is_null($value) && !is_array($value)) {
+            return false;
+        }
+
+        $relation = $this->schema->relationship($attribute);
+
+        if ($relation instanceof PolymorphicRelation) {
+            $this->types = $relation->inverseTypes();
+        } else {
+            $this->types = [$relation->inverse()];
+        }
+
+        return $this->accept($value);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        $key = 'jsonapi::validation.' . Str::snake(class_basename($this));;
+
+        return trans($key, [
+            'types' => collect($this->types)->implode(', '),
+        ]);
+    }
+
+    /**
+     * Accept the data value.
+     *
+     * @param array|null $data
+     * @return bool
+     */
+    protected function accept(?array $data): bool
+    {
+        if (is_null($data)) {
+            return true;
+        }
+
+        return $this->acceptType($data);
+    }
+
+    /**
+     * @param $data
+     * @return bool
+     */
+    protected function acceptType($data): bool
+    {
+        return is_array($data) && collect($this->types)->containsStrict(
+            $data['type'] ?? null
+        );
+    }
+
+}
---
+++ tests/Unit/Rules/AllowedIncludePathsTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2020 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace LaravelJsonApi\Validation\Tests\Unit\Rules;
+
+use LaravelJsonApi\Contracts\Schema\Schema;
+use LaravelJsonApi\Validation\Rules\AllowedIncludePaths;
+use PHPUnit\Framework\TestCase;
+
+class AllowedIncludePathsTest extends TestCase
+{
+
+    public function test(): void
+    {
+        $rule = new AllowedIncludePaths(['foo', 'bar']);
+
+        $this->assertTrue($rule->passes('include', 'foo,bar'));
+        $this->assertFalse($rule->passes('include', 'foo,baz'));
+    }
+
+    public function testWithMethods(): void
+    {
+        $rule = (new AllowedIncludePaths())
+            ->allow('foo', 'bar', 'id')
+            ->forget('id');
+
+        $this->assertTrue($rule->passes('include', 'foo,bar'));
+        $this->assertFalse($rule->passes('include', 'foo,baz'));
+    }
+
+    public function testSchema(): void
+    {
+        $schema = $this->createMock(Schema::class);
+        $schema->method('includePaths')->willReturn(['author', 'comments']);
+
+        $this->assertEquals(
+            new AllowedIncludePaths(['author', 'comments']),
+            AllowedIncludePaths::make($schema)
+        );
+    }
+
+}
